f4899dcb3d39f4d6a272048fee23026b4839675c
Anton <Kondrahin210@gmail.com>
2021-11-18 20:59:34 +0300

    :sparkles: Add get_token adapter

---
+++ app/settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    """Main settings."""
+
+    class Config:
+        env_file = ".env"
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+
+config = AppSettings()
